0c28b9c6da812df23795c6ff408ce55759e311ed
Jexy-Kynner <3145227258@qq.com>
2024-10-24 11:45:23 +0800

    Site updated: 2024-10-24 11:45:23

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@
 
 'use strict';
 
-var precacheConfig = [];
+var precacheConfig = [["/sw-register.js","e9f7e71777bbf175b69d3a19e4f7756c"]];
 var cacheName = 'sw-precache-v3--' + (self.registration ? self.registration.scope : '');
 var firstRegister = 1; // 默认1是首次安装SW， 0是SW更新
 
